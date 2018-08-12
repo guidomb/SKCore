dc261ee9f41480ecdb3608938d9c3be123b03b9c
Guido Marucci Blas <guidomb@gmail.com>
2018-08-11 20:20:27 -0500

    Add locale filed to User.Profile

--- Sources/SKCore/User.swift
+++ Sources/SKCore/User.swift
@@ -34,6 +34,7 @@ public struct User {
         public var image72: String?
         public var image192: String?
         public var customProfile: CustomProfile?
+        public var locale: String?
 
         public init(profile: [String: Any]?) {
             firstName = profile?["first_name"] as? String
@@ -47,6 +48,7 @@ public struct User {
             image72 = profile?["image_72"] as? String
             image192 = profile?["image_192"] as? String
             customProfile = CustomProfile(customFields: profile?["fields"] as? [String: Any])
+            locale = profile?["locale"] as? String
         }
     }
 
